f324539660f2da9cd058d49387b790f8288b197c
Lailatul Qadri <107826331+kadriputra@users.noreply.github.com>
2023-03-03 09:31:46 +0700

    Add files via upload

---
+++ data_mahasantri.blade.php
@@ -0,0 +1,33 @@
+<h3>Data Mahasiswa</h3>
+<table border="1">
+    <tr>
+        <th>No</th>
+        <th>Nama</th>
+        <th>Alamat</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>{{ $mhs1 }}</td>
+        <td>{{ $asal1 }}</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>{{ $mhs2 }}</td>
+        <td>{{ $asal2 }}</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>{{ $mhs3 }}</td>
+        <td>{{ $asal3 }}</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>{{ $mhs4 }}</td>
+        <td>{{ $asal4 }}</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>{{ $mhs5 }}</td>
+        <td>{{ $asal5 }}</td>
+    </tr>
+</table>
---
+++ web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasantriController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Url Salam
+Route::get('/salam',function(){
+    return "Assalamu'alaikum Selamat Pagi Dunia";
+});
+
+// Route dengan Parameter
+Route::get('/pegawai/{nama}/{divisi}',function($nama,$divisi){
+    return "Nama Pegawai : ".$nama."<br/>Departemen : ".$divisi;
+});
+
+// Route dengan Redirect Page Views
+Route::get('/kabar', function () {
+    return view('kondisi');
+}); 
+
+// Route /mahasantri
+Route::get('/mahasantri',[MahasantriController::class, 'dataMahasantri']);
+
+
